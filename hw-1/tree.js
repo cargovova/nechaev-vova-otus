a40c5df3a08309688e8f6c0dd59dd14435eb8754
cargovova <cargovova@outlook.com>
2021-07-09 11:26:08 +0400

    подготовка массива для отрисовки

--- hw-1/tree.js
+++ hw-1/tree.js
@@ -1,6 +1,66 @@
+const fs = require('fs')
+
 const args = process.argv.slice(2)
 let path = null
 let depth = null
+let folders = []
+
+scaner = (folder_path) => {
+  let elements = fs.readdirSync(folder_path)
+  elements.forEach(element => {
+    let stat = fs.lstatSync(folder_path + '\\' + element)
+    if (stat.isDirectory()) {
+      let path = folder_path + '\\' + element
+      folders.push(path)
+      scaner(path)
+    }
+  })
+}
+
+cutForDepth = (folders_path, path) => {
+  let cuted_folders_path = []
+  folders_path.forEach(folder => {
+    let pos = path.length
+    let i = 0
+    while (i <= depth) {
+      i = i + 1
+      var foundPos = folder.indexOf('\\', pos)
+      if (foundPos === -1) break
+      pos = foundPos + 1
+    }
+    finish_path = foundPos !== -1 ? folder.slice(0, pos - 1) : folder
+    cuted_folders_path.push(finish_path)
+  })
+  return cuted_folders_path
+}
+
+uniqueFoldersPath = (folders_path) => {
+  let uniqueFolders = []
+  folders_path.forEach(folder => {
+    if (!~uniqueFolders.indexOf(folder)) {
+      uniqueFolders.push(folder)
+    }
+  })
+  return uniqueFolders
+}
+
+splitElements = (path, files) => {
+  let directory = { files: [], folders: [] }
+  files.forEach(element => {
+    fs.lstatSync(path + '\\' + element).isDirectory()
+      ? directory.folders.push(element)
+      : directory.files.push(element)
+  })
+  return directory
+}
+
+collectObject = (path_to_folder) => {
+  const files = fs.readdirSync(path_to_folder)
+  const element = splitElements(path_to_folder, files)
+  element.path = path_to_folder
+  return element
+}
+
 if (args[0] === '--help') {
   console.log("Утилита для просмотра структуры каталога")
   console.log("node tree.js [-d, --depth глубина структуры] [путь до файла]")
@@ -15,10 +75,20 @@ if (args[0] === '--help') {
   path = args[0]
   if (!path || !depth || !parseInt(depth, 10)) {
     console.log("Аргументы введены не корректно \n Введите 'node tree.js --help' для справки")
+  } else {
+    let directory = []
+    // рекурсивный поиск всех путей до папок
+    scaner(path)
+    // обрезка путей до нужной глубины
+    const cutted_folders_path = cutForDepth(folders, path)
+    // уникализация путей
+    const unique_folders_path = uniqueFoldersPath(cutted_folders_path)
+    // массив с готовыми для отрисовки объектами
+    unique_folders_path.forEach(path => {
+      directory.push(collectObject(path))
+    })
+    console.log(directory)
   }
 } else {
   console.log("Необходимо указать три аргумента! \n Введите 'node tree.js --help' для справки")
 }
-
-console.log(path)
-console.log(depth)
