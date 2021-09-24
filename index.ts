74da9b332edb07daf84f55c4d1cdc2b82a808785
cargovova <cargovova@outlook.com>
2021-09-24 17:36:22 +0400

    feat add arg processing and help

--- index.ts
+++ index.ts
@@ -2,10 +2,6 @@ import { BaseExternalAccountClient, Compute, Impersonated, JWT, UserRefreshClien
 import { google, Auth, sheets_v4 } from 'googleapis'
 import GetSheetDto from './dtos/get-sheet-dto'
 
-const ACCESSIBLE_SHEETS_IDS: Array<string> = [
-  '16NjI46MLajsBGZuR0Eh8GKzSyYBAJLkEo2tA4SDVdOs', '16ArJIu5hgrCvFG9G5FrG4rLmiB2H9IrXZScV0hMMr68'
-]
-
 const sheets: sheets_v4.Sheets = google.sheets('v4');
 type ClientType = Compute | JWT | UserRefreshClient | Impersonated | BaseExternalAccountClient
 
@@ -60,15 +56,48 @@ const updateSheetsTitle = async (sheetsIds: Array<string>, newTitle: string): Pr
 }
 
 const start = async (): Promise<void> => {
-  try {
+  const args: Array<string> = process.argv.slice(2)
+  let spreadsheetIds: Array<string> = []
+  let newTitle: string = ''
+  if (args[0] === '--help' || args[0] === '-h') {
+    console.log("CLI для чтения и изменения названий google таблиц")
+    console.log("node tree.ts [-spr id таблицы]")
+    console.log("node tree.ts [-spr id таблицы] [-nt новое имя таблицы]")
+    console.log('Примеры использования: \n ' +
+      'node tree.ts -spr 16NjI46MLajsBGZuR0Eh8GKzSyYBAJLkEo2tA4SDVdOt \n ' +
+      'node tree.ts -spr 16NjI46MLajsBGZuR0Eh8GKzSyYBAJLkEo2tA4SDVdOt,16NjI46MLajsBGZuR0Eh8GKzSyYBAJLkEo2tA4SDVdOt \n ' +
+      'node tree.ts -spr 16NjI46MLajsBGZuR0Eh8GKzSyYBAJLkEo2tA4SDVdOt -nt newname' +
+      'node tree.ts -spr 16NjI46MLajsBGZuR0Eh8GKzSyYBAJLkEo2tA4SDVdOt,16NjI46MLajsBGZuR0Eh8GKzSyYBAJLkEo2tA4SDVdOt -nt newname')
+  } else if (args.length) {
     const authClient: ClientType = await getGoogleClient()
     google.options({ auth: authClient })
-    const sheets = await getSheets(ACCESSIBLE_SHEETS_IDS)
-    console.log(sheets)
-    const updatedSheets = await updateSheetsTitle(ACCESSIBLE_SHEETS_IDS, 'title4')
-    console.log(updatedSheets)
-  } catch (error) {
-    console.log(error)
+    switch (args.length) {
+      case 2:
+        try {
+          if (args[1]) {
+            spreadsheetIds = args[1]?.split(',')
+            const sheets = await getSheets(spreadsheetIds)
+            console.log(sheets)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+        break;
+      case 4:
+        try {
+          if (args[1] && args[0] === '-spr' && args[3] && args[2] === '-nt') {
+            spreadsheetIds = args[1]?.split(',')
+            newTitle = args[3]
+            const updatedSheets = await updateSheetsTitle(spreadsheetIds, newTitle)
+            console.log(updatedSheets)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+        break;
+    }
+  } else {
+    console.log("Аргументы введены не корректно \n Введите 'node tree.ts --help' для справки")
   }
 }
 
